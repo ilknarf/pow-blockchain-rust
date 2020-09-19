1674e8b32940d0694ea7192abc3d8010eab12dd0
Frank <lifrank@grinnell.edu>
2020-09-19 11:06:32 -0500

    added half of mine function

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,24 @@ extern crate crypto; // aka rust-crypto
 use std::io::{Cursor, Write};
 use std::fmt::{Display, Formatter, Result};
 use std::str;
+use std::u64;
 use std::boxed::Box;
 use std::option::Option;
 use self::crypto::digest::Digest;
 use self::crypto::sha3::Sha3;
 use hex::encode;
-use byteorder::{LittleEndian, ReadBytesExt};
 
-const TARGET: u32 = 5; // target number zeroes
+const TARGET: u32 = 6; // target number zeroes
 
-// 14 byte - number || 64 byte - last block hash || 64-byte - merkle root || 20 byte - data || 18 byte - nonce || total 180 bytes
+// 14 byte - number || 64 byte - last block hash || 64-byte - merkle root || 22 byte - data || 16 byte - nonce || total 180 bytes
 // merkle root = sha3(block + last_merkle_root)
 
 const GENESIS_BLOCK: Block = Block{
     number: [0u8; 14],
     last_block_hash: [0u8; 64],
     merkle_root: [0u8; 64],
-    data: [0u8; 20],
-    nonce: [0u8; 18],
+    data: [0u8; 22],
+    nonce: [0u8; 16],
     last_block: None,
 };
 
@@ -37,9 +37,9 @@ fn main() {
             number: last_block.next_number(),
             last_block_hash,
             merkle_root,
-            data: [0u8; 20],
+            data: [0u8; 22],
             last_block: Option::from(Box::new(last_block)),
-            nonce: [0u8; 18],
+            nonce: [0u8; 16],
         };
 
         println!("{}", last_block);
@@ -52,8 +52,8 @@ struct Block{
     number: [u8; 14], // len 14
     last_block_hash: [u8; 64], // len 64
     merkle_root: [u8; 64], // len 64
-    data: [u8; 20], // len 20
-    nonce: [u8; 18], // len 18
+    data: [u8; 22], // len 22
+    nonce: u128, // len 16
     last_block: Link,
 }
 
@@ -65,26 +65,40 @@ impl Block {
         sha3.input(&self.last_block_hash);
         sha3.input(&self.merkle_root);
         sha3.input(&self.data);
-        sha3.input(&self.nonce);
+        sha3.input(&self.nonce.to_be_bytes());
 
         let mut result = [0u8; 64];
         sha3.result(&mut result);
         result
     }
 
+    // next_number actually copies then uncopies from 16-byte arrays; unfortunately
+    // requires 16-length arrays to convert
     fn next_number(&self) -> [u8; 14] {
-        let mut v = vec![0, 0];
-        v.extend(&self.number);
+        let mut a = [0u8; 16];
 
-        let mut cur = Cursor::new(v);
-        let next_num = cur.read_u128::<LittleEndian>().unwrap();
+        for i in 2..14 {
+            a[i] = self.number[i - 2];
+        }
+
+        let next_num = u128::from_be_bytes(a) + 1;
+        println!("{:?}", next_num.to_le_bytes());
 
         let mut res = [0u8; 14];
-        for (i, number) in next_num.to_le_bytes()[2..].iter().enumerate() {
+        for (i, number) in next_num.to_be_bytes()[1..14].iter().enumerate() {
             res[i] = *number;
         }
 
         res
+    }
+
+    fn mine(&self) {
+
+        for i in 0..u128::max() {
+            self.nonce++;
+
+            if self.hash()
+        }
     }
 }
 
@@ -97,12 +111,13 @@ impl Display for Block {
 
 
         f.write_fmt(format_args!(
-            "number: {}, last_block: {}{}, merkle_root: {}{}, data: {}",
+            "number: {}, last_block: {}{}, merkle_root: {}{}, nonce: {}, data: {}",
             encode(&self.number),
             encode(&self.last_block_hash[..32]),
             encode(&self.last_block_hash[32..]),
             encode(&self.merkle_root[..32]),
             encode(&self.merkle_root[32..]),
+            &self.nonce,
             data_string,
         ))
     }
